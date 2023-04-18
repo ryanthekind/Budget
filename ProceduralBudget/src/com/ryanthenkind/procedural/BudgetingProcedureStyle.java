b97d47e61602af396e1c56e9ffb67fa06296fba1
Ryan Burns <mr.ryan.burns@outlook.com>
2023-04-17 21:41:17 -0400

    added if else statement

--- ProceduralBudget/src/com/ryanthenkind/procedural/BudgetingProcedureStyle.java
+++ ProceduralBudget/src/com/ryanthenkind/procedural/BudgetingProcedureStyle.java
@@ -2,7 +2,9 @@ package com.ryanthenkind.procedural;
 
 /**
  * Procedural Budgeting App that demonstrates how an app can function
- * without the use of other classes
+ * without the use of other classes. You will see in the code below that
+ * though procedural programming makes sense in simple programs OOP 
+ * is preferable when making a program more dynamic
  * @author ryanb
  *
  */
@@ -14,7 +16,7 @@ public class BudgetingProcedureStyle {
 		// variables for personA
 		String personA = "Ryan";
 		int monthlyIncomeA = 2000;
-		int monthlyExpensesA = 1800;
+		int monthlyExpensesA = 1500;
 		
 		double monthlyAssetsA = monthlyIncomeA - monthlyExpensesA;
 		
@@ -33,17 +35,28 @@ public class BudgetingProcedureStyle {
 		
 		// output the result for personA
 		System.out.printf("%s takes home $%d every month. \n", personA, monthlyIncomeA);
-		System.out.printf("%s's monthly expenses are $%d leaving him with $%.2f at the every month\n", personA, monthlyExpensesA, monthlyAssetsA);
+		System.out.printf("%s's monthly expenses are $%d leaving him with $%.2f at the end of every month\n", personA, monthlyExpensesA, monthlyAssetsA);
 		System.out.printf("%s nets $%.2f every year. \n", personA, annualTakeHomeA);
 		
 		System.out.println();
 		System.out.println("<------------> \n");
 		// output result for personB
 		System.out.printf("%s takes home $%d every month. \n", personB, monthlyIncomeB);
-		System.out.printf("%s's monthly expenses are $%d leaving him with $%.2f at the every month\n", personB, monthlyExpensesB, monthlyAssetsB);
+		System.out.printf("%s's monthly expenses are $%d leaving him with $%.2f at the end of every month\n", personB, monthlyExpensesB, monthlyAssetsB);
 		System.out.printf("%s nets $%.2f every year. \n", personB, annualTakeHomeB);
 		
-
+		
+		// If else construct 
+		if (monthlyAssetsA == monthlyAssetsB) {
+			System.out.printf("%s and %s have the same money left over at the end of the month", personA, personB);
+		} else if ( monthlyAssetsA > monthlyAssetsB) {
+			double remainder = monthlyAssetsA - monthlyAssetsB;
+			System.out.printf("%s has moe money than %s by $%.2f at the end of the month", personA, personB, remainder);
+		} else {
+			double remainder = monthlyAssetsB - monthlyAssetsA;
+			System.out.printf("%s has more money than %s by $%.2f at the end of the month", personB, personA, remainder);
+		}
+		
 	}
 
 }
