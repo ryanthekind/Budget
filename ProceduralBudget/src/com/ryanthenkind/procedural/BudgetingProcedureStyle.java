a6c6b46cb6323199a3eb2c51455dbced73ac49a0
Ryan Burns <mr.ryan.burns@outlook.com>
2023-04-17 21:24:07 -0400

    updated procedural code example

--- ProceduralBudget/src/com/ryanthenkind/procedural/BudgetingProcedureStyle.java
+++ ProceduralBudget/src/com/ryanthenkind/procedural/BudgetingProcedureStyle.java
@@ -11,13 +11,38 @@ public class BudgetingProcedureStyle {
 
 	public static void main(String[] args) {
 		
+		// variables for personA
 		String personA = "Ryan";
 		int monthlyIncomeA = 2000;
+		int monthlyExpensesA = 1800;
+		
+		double monthlyAssetsA = monthlyIncomeA - monthlyExpensesA;
 		
 		double annualTakeHomeA = monthlyIncomeA * 12;
 		
-		System.out.printf("%s takes home $%d every month. \n", personA, monthlyIncomeA );
+		
+		// variables for personB
+		String personB = "Jeff";
+		int monthlyIncomeB = 2400;
+		int monthlyExpensesB = 2000;
+		
+		double monthlyAssetsB = monthlyIncomeB - monthlyExpensesB;
+		
+		double annualTakeHomeB = monthlyIncomeB * 12;
+		
+		
+		// output the result for personA
+		System.out.printf("%s takes home $%d every month. \n", personA, monthlyIncomeA);
+		System.out.printf("%s's monthly expenses are $%d leaving him with $%.2f at the every month\n", personA, monthlyExpensesA, monthlyAssetsA);
 		System.out.printf("%s nets $%.2f every year. \n", personA, annualTakeHomeA);
+		
+		System.out.println();
+		System.out.println("<------------> \n");
+		// output result for personB
+		System.out.printf("%s takes home $%d every month. \n", personB, monthlyIncomeB);
+		System.out.printf("%s's monthly expenses are $%d leaving him with $%.2f at the every month\n", personB, monthlyExpensesB, monthlyAssetsB);
+		System.out.printf("%s nets $%.2f every year. \n", personB, annualTakeHomeB);
+		
 
 	}
 
